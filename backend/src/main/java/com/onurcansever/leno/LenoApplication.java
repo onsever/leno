287bd42004b35e5d14940cebf0f8c3e6e791718a
Onurcan Sever <onsever@icloud.com>
2023-08-28 15:47:27 -0400

    project bootstrap with spring

---
+++ backend/src/main/java/com/onurcansever/leno/LenoApplication.java
@@ -0,0 +1,13 @@
+package com.onurcansever.leno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/onurcansever/leno/LenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurcansever.leno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
