9a6c852428393e2a35514b97596ff61e5d28b12c
Onurcan Sever <onsever@icloud.com>
2023-08-29 15:01:22 -0400

    add annotations for enabling jpa auditing and jpa repositories. create a bean for modelmapper in the spring context

--- backend/src/main/java/com/onurcansever/leno/LenoApplication.java
+++ backend/src/main/java/com/onurcansever/leno/LenoApplication.java
@@ -1,10 +1,23 @@
 package com.onurcansever.leno;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EntityScan(value = "com.onurcansever.leno.entity")
+@EnableJpaRepositories(value = "com.onurcansever.leno.repository")
 public class LenoApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LenoApplication.class, args);
