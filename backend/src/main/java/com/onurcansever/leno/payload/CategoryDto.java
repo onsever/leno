af76953c7ab811cbc92554ffc7ac3a8ae2f249a9
Onurcan Sever <onsever@icloud.com>
2023-08-30 13:19:33 -0400

    create dto class for category

---
+++ backend/src/main/java/com/onurcansever/leno/payload/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.onurcansever.leno.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public final class CategoryDto {
+    private Long categoryId;
+    private String name;
+}
