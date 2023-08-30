175100340cb0eeccbd610e1126c1af09a320ea14
Onurcan Sever <onsever@icloud.com>
2023-08-30 13:21:26 -0400

    insert categories to the product detail dto response

--- backend/src/main/java/com/onurcansever/leno/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/com/onurcansever/leno/service/impl/ProductServiceImpl.java
@@ -3,6 +3,8 @@ package com.onurcansever.leno.service.impl;
 import com.onurcansever.leno.entity.Customer;
 import com.onurcansever.leno.entity.Product;
 import com.onurcansever.leno.exception.ResourceNotFoundException;
+import com.onurcansever.leno.payload.CategoryDto;
+import com.onurcansever.leno.payload.CustomerDto;
 import com.onurcansever.leno.payload.ProductDetailDto;
 import com.onurcansever.leno.payload.ProductDto;
 import com.onurcansever.leno.repository.CustomerRepository;
@@ -14,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -50,9 +53,11 @@ public class ProductServiceImpl implements ProductService {
     public ProductDetailDto getProductDetail(Product product, Customer customer) {
         Product existingProduct = this.productRepository.findById(product.getProductId()).orElseThrow(() -> new ResourceNotFoundException("Product", "productId", product.getProductId()));
 
-        Customer existingCustomer = existingProduct.getCustomer();
+        CustomerDto existingCustomer = this.modelMapper.map(customer, CustomerDto.class);
 
-        return new ProductDetailDto(existingProduct, existingCustomer);
+        Set<CategoryDto> categories = existingProduct.getCategories().stream().map(category -> this.modelMapper.map(category, CategoryDto.class)).collect(Collectors.toSet());
+
+        return new ProductDetailDto(existingProduct, existingCustomer, categories);
     }
 
     @Override
