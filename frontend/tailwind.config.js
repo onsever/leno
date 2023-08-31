9b90edd859613a78863e451487634348b8d7d3ee
Onurcan Sever <onsever@icloud.com>
2023-08-31 12:12:36 -0400

    extend utility classes of the tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#077782",
+      },
+      fontFamily: {
+        chewy: ["Chewy", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
