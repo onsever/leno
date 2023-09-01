b24324debd1da99907a50cef4ecefdef77318e77
Onurcan Sever <onsever@icloud.com>
2023-09-01 12:21:21 -0400

    change title of the document for the feed page

--- frontend/src/pages/FeedPage/index.tsx
+++ frontend/src/pages/FeedPage/index.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export default function FeedPage() {
+  useEffect(() => {
+    document.title = "Secondhand fashion on Leno | Leno";
+  }, []);
+
   return (
     <div>
       <h1>Feed Page</h1>
