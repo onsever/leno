567bc786d38fd6c7c70cfb5b06d6ba1aa505ec92
Onurcan Sever <onsever@icloud.com>
2023-08-31 23:17:15 -0400

    export feed page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as HomePage } from "./HomePage";
 export { default as RegisterPage } from "./Auth/RegisterPage";
 export { default as LoginPage } from "./Auth/LoginPage";
 export { default as ErrorPage } from "./ErrorPage";
+export { default as FeedPage } from "./FeedPage";
