29ac9c7a334196791a50e30847e97d7eabfe2ee9
Onurcan Sever <onsever@icloud.com>
2023-08-31 22:36:35 -0400

    add types for JWT token

--- frontend/src/types/index.tsx
+++ frontend/src/types/index.tsx
@@ -12,4 +12,16 @@ type LoginCredentials = {
   password: string;
 };
 
-export type { RegisterCredentials, LoginCredentials };
+type JWT = {
+  accessToken: string;
+  tokenType: string;
+};
+
+type JWTReturn = {
+  customerId: number;
+  sub: string;
+  iat: number;
+  exp: number;
+};
+
+export type { RegisterCredentials, LoginCredentials, JWT, JWTReturn };
