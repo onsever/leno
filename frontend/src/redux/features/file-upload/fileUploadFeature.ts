79e55672c9045d23a43132b2509711f18dadfda1
Onurcan Sever <onsever@icloud.com>
2023-09-01 23:49:51 -0400

    implement address, customer, and file upload features

---
+++ frontend/src/redux/features/file-upload/fileUploadFeature.ts
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../../store.ts";
+
+export const fileUploadApi = createApi({
+  reducerPath: "fileUploadApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_URL}/upload`,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      headers.set("Content-Type", "multipart/form-data");
+      headers.set("Access-Control-Allow-Origin", "*");
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    uploadProfilePicture: builder.mutation({
+      query: ({ file, customerId }) => {
+        const formData = new FormData();
+
+        formData.append("file", file);
+        formData.append("customerId", customerId);
+
+        console.log(formData.get("file"));
+        console.log(formData.get("customerId"));
+
+        return {
+          url: "/customer/profile-picture",
+          method: "POST",
+          body: formData,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useUploadProfilePictureMutation } = fileUploadApi;
