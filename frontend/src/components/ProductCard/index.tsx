fca21c28254ff18f75cf9aa3d161057d998de4ab
Onurcan Sever <onsever@icloud.com>
2023-09-02 14:33:15 -0400

    create product card component to display product information

---
+++ frontend/src/components/ProductCard/index.tsx
@@ -0,0 +1,34 @@
+import { ProductDetail } from "../../types";
+import { useNavigate } from "react-router-dom";
+
+interface ProductCardProps {
+  product: ProductDetail;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/products/${product.productId}`);
+  };
+  return (
+    <div
+      className="flex flex-col justify-between w-full h-full cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="flex flex-col justify-between w-full h-full">
+        <div className="flex justify-center items-center w-full h-60">
+          <img
+            src={product.image}
+            alt="product"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-start w-full mt-2">
+          <span className="text-lg font-bold">{product.name}</span>
+          <span className="text-sm text-gray-500">{product.description}</span>
+          <span className="text-lg font-bold">{product.price}$</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -4,3 +4,4 @@ export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as Modal } from "./Modal";
 export { default as Loading } from "./Loading";
+export { default as ProductCard } from "./ProductCard";
