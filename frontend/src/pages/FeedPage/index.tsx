e4e15c4d22e59825036d15d568af9378d439ee49
Onurcan Sever <onsever@icloud.com>
2023-09-02 21:21:31 -0400

    implement feed page

--- frontend/src/pages/FeedPage/index.tsx
+++ frontend/src/pages/FeedPage/index.tsx
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
+import { useGetAllProductsQuery } from "../../redux/features/product/productFeature.ts";
+import { FeedProductCard, Loading } from "../../components";
 
 export default function FeedPage() {
+  const { data: products, isLoading } = useGetAllProductsQuery(0);
+
+  console.log(products);
+
   useEffect(() => {
     document.title = "Secondhand fashion on Leno | Leno";
   }, []);
 
+  if (isLoading) return <Loading />;
+
   return (
-    <div>
-      <h1>Feed Page</h1>
-    </div>
+    <section className="flex flex-col mx-auto w-[1120px] py-10 pb-32">
+      <h1 className="text-2xl font-medium mb-4">Feed</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {products?.map((product) => (
+          <FeedProductCard product={product} key={product.productId} />
+        ))}
+      </div>
+    </section>
   );
 }
