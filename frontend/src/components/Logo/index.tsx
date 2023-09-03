9e155e6581b475e53d39e7ee6fc00726c776093a
Onurcan Sever <onsever@icloud.com>
2023-09-02 21:20:24 -0400

    navigate to different route

--- frontend/src/components/Logo/index.tsx
+++ frontend/src/components/Logo/index.tsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store.ts";
 
 export default function Logo() {
+  const token = useSelector((state: RootState) => state.auth.token);
+
   return (
-    <Link to={"/"}>
+    <Link to={`${token ? "/feed" : "/"}`}>
       <h1 className="font-chewy text-primary text-4xl">Leno</h1>
     </Link>
   );
