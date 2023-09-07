8334b907676206235b277c8b67d75e4319f25aa0
Onurcan Sever <onsever@icloud.com>
2023-09-06 20:32:07 -0400

    implement routing for breadcrumb links

--- frontend/src/components/Breadcrumb/index.tsx
+++ frontend/src/components/Breadcrumb/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { ProductDetail } from "../../types";
 
 interface BreadcrumbProps {
@@ -5,6 +6,8 @@ interface BreadcrumbProps {
 }
 
 export default function Breadcrumb({ product }: BreadcrumbProps) {
+  const navigate = useNavigate();
+
   const mainCategory = product?.categories.find(
     (category) =>
       category.name === "Men" ||
@@ -16,15 +19,45 @@ export default function Breadcrumb({ product }: BreadcrumbProps) {
     (category) => category.name !== mainCategory?.name
   );
 
+  const renameCategory = (category: string) => {
+    return category.split(" ")[0].toLowerCase();
+  };
+
   return (
     <div className="flex items-center text-gray-500">
-      <span>Home</span>
+      <span
+        className="cursor-pointer hover:underline"
+        onClick={() => navigate("/")}
+      >
+        Home
+      </span>
       <span className="mx-2">/</span>
-      <span>Products</span>
+      <span
+        className="cursor-pointer hover:underline"
+        onClick={() => navigate("/feed")}
+      >
+        Products
+      </span>
       <span className="mx-2">/</span>
-      <span>{mainCategory?.name}</span>
+      <span
+        className="cursor-pointer hover:underline"
+        onClick={() => navigate(`/${renameCategory(mainCategory!.name)}`)}
+      >
+        {mainCategory?.name}
+      </span>
       <span className="mx-2">/</span>
-      <span>{subCategory?.name}</span>
+      <span
+        className="cursor-pointer hover:underline"
+        onClick={() =>
+          navigate(
+            `/${renameCategory(mainCategory!.name)}?sub=${renameCategory(
+              subCategory!.name
+            )}`
+          )
+        }
+      >
+        {subCategory?.name}
+      </span>
       <span className="mx-2">/</span>
       <span>{product?.name}</span>
     </div>
